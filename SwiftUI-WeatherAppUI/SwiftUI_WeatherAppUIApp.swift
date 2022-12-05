ca4dc37a607b224882b01ff4c616e1083ae1f072
Swapnil <65143129+swapnilAndhale@users.noreply.github.com>
2022-12-05 15:10:57 +0530

    Initial Commit

---
+++ SwiftUI-WeatherAppUI/SwiftUI_WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppUIApp.swift
+//  SwiftUI-WeatherAppUI
+//
+//  Created by macos on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
