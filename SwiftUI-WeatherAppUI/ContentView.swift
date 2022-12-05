bb089d3410f4968748b357a0d2b31e024d4ee92d
Swapnil <65143129+swapnilAndhale@users.noreply.github.com>
2022-12-05 15:38:17 +0530

    text added

--- SwiftUI-WeatherAppUI/ContentView.swift
+++ SwiftUI-WeatherAppUI/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Pune, MH")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                    Spacer()
+            }
         }
-        .padding()
     }
 }
 
